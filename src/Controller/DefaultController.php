6d7ec538bde98ce9bb399f7c72ee40bd31019e2b
Ivan <ivan.melnichuk@onyxenterprises.com>
2019-02-05 11:25:17 +0200

    add ipa for default controller

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -19,6 +19,40 @@ class DefaultController extends AbstractController
     public function index(UserDataProvider $userDataProvider, HolidayService $holidayService)
     {
 //        $time = $userDataProvider->getUserTime('ivan.melnichuk', '01/01/2019', '31/01/2019');
+
+        $username = 'ivan.melnichuk';
+        $username = trim($username);
+        $ldappass = '!rCLV@HVbFkV4p7fNFz4';
+        $ldappass = trim($ldappass);
+
+        $ldaprdn = 'uid=' . $username . ',cn=users,cn=accounts,dc=onyxenterprises,dc=com';
+        $ldapconn = ldap_connect("ipa1.onyxenterprises.com", '389')
+        or die("Unavailable to connect LDAP.");
+        if ($ldapconn) {
+            ldap_set_option($ldapconn, LDAP_OPT_PROTOCOL_VERSION, 3);
+            ldap_set_option($ldapconn, LDAP_OPT_REFERRALS, 0);
+
+            $bind = @ldap_bind($ldapconn, $ldaprdn, $ldappass);
+        }
+        var_dump($bind);
+        $ldapconn = ldap_connect("ipa2.onyxenterprises.com", '389')
+        or die("Unavailable to connect LDAP.");
+        if ($ldapconn) {
+            ldap_set_option($ldapconn, LDAP_OPT_PROTOCOL_VERSION, 3);
+            ldap_set_option($ldapconn, LDAP_OPT_REFERRALS, 0);
+
+            $bind = @ldap_bind($ldapconn, $ldaprdn, $ldappass);
+        }
+        var_dump($bind);
+        $ldapconn = ldap_connect("ipa.onyxenterprises.com", '389')
+        or die("Unavailable to connect LDAP.");
+        if ($ldapconn) {
+            ldap_set_option($ldapconn, LDAP_OPT_PROTOCOL_VERSION, 3);
+            ldap_set_option($ldapconn, LDAP_OPT_REFERRALS, 0);
+
+            $bind = @ldap_bind($ldapconn, $ldaprdn, $ldappass);
+        }
+        var_dump($bind);
         $dateTime = new \DateTime();
         return $this->render('default/index.html.twig', [
             'controller_name' => 'DefaultController',
