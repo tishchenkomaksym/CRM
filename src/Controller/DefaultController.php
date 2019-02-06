3c78665c3564319cd914415025e8a909c11891bf
Ivan <ivan.melnichuk@onyxenterprises.com>
2019-02-06 09:09:27 +0200

    add ldap authentification

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -14,6 +14,7 @@ class DefaultController extends AbstractController
     }
 
     /**
+     *
      * @Route("/default", name="default")
      */
     public function index(UserDataProvider $userDataProvider, HolidayService $holidayService)
