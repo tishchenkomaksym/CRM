15a5a7a5c922224116095a7efe23552a4a77f4f1
ivan.melnichuk <ivan.melnichuk@onyx.com>
2019-04-09 10:53:22 +0300

    Task: https://jira.onyxenterprises.com/browse/WT-65 Description: add qa components

---
+++ src/Entity/QaComponents.php
@@ -0,0 +1,142 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\QaComponentsRepository")
+ */
+class QaComponents
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=1000)
+     */
+    private $childComponents;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $firstLvlHours;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $secondLvlHours;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $thirdLvlHours;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $fourLvlHours;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $fiveLvlHours;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getChildComponents(): ?string
+    {
+        return $this->childComponents;
+    }
+
+    public function setChildComponents(string $childComponents): self
+    {
+        $this->childComponents = $childComponents;
+
+        return $this;
+    }
+
+    public function getFirstLvlHours(): ?int
+    {
+        return $this->firstLvlHours;
+    }
+
+    public function setFirstLvlHours(int $firstLvlHours): self
+    {
+        $this->firstLvlHours = $firstLvlHours;
+
+        return $this;
+    }
+
+    public function getSecondLvlHours(): ?int
+    {
+        return $this->secondLvlHours;
+    }
+
+    public function setSecondLvlHours(int $secondLvlHours): self
+    {
+        $this->secondLvlHours = $secondLvlHours;
+
+        return $this;
+    }
+
+    public function getThirdLvlHours(): ?int
+    {
+        return $this->thirdLvlHours;
+    }
+
+    public function setThirdLvlHours(int $thirdLvlHours): self
+    {
+        $this->thirdLvlHours = $thirdLvlHours;
+
+        return $this;
+    }
+
+    public function getFourLvlHours(): ?int
+    {
+        return $this->fourLvlHours;
+    }
+
+    public function setFourLvlHours(int $fourLvlHours): self
+    {
+        $this->fourLvlHours = $fourLvlHours;
+
+        return $this;
+    }
+
+    public function getFiveLvlHours(): ?int
+    {
+        return $this->fiveLvlHours;
+    }
+
+    public function setFiveLvlHours(int $fiveLvlHours): self
+    {
+        $this->fiveLvlHours = $fiveLvlHours;
+
+        return $this;
+    }
+}
---
+++ src/Migrations/Version20190409074309.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\DBALException;
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20190409074309 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    /**
+     * @param Schema $schema
+     * @throws DBALException
+     */
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE TABLE qa_components (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, child_components VARCHAR(1000) NOT NULL, first_lvl_hours INT NOT NULL, second_lvl_hours INT NOT NULL, third_lvl_hours INT NOT NULL, four_lvl_hours INT NOT NULL, five_lvl_hours INT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+    }
+
+    /**
+     * @param Schema $schema
+     * @throws DBALException
+     */
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('DROP TABLE qa_components');
+    }
+}
