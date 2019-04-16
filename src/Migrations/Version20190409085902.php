b650793df47f4412be5760f1b2cb6858737b795e
ivan.melnichuk <ivan.melnichuk@onyx.com>
2019-04-16 18:09:47 +0300

    Task: https://jira.onyxenterprises.com/browse/WT-65 Description: add mmore symbols

--- src/Migrations/Version20190409085902.php
+++ src/Migrations/Version20190409085902.php
@@ -22,7 +22,7 @@ final class Version20190409085902 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('CREATE TABLE qa_components (id INT AUTO_INCREMENT NOT NULL, team_id INT NOT NULL, name VARCHAR(255) NOT NULL, child_components VARCHAR(1000) NOT NULL, first_lvl_hours INT NOT NULL, second_lvl_hours INT NOT NULL, third_lvl_hours INT NOT NULL, four_lvl_hours INT NOT NULL, five_lvl_hours INT NOT NULL, INDEX IDX_3D65778D296CD8AE (team_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE qa_components (id INT AUTO_INCREMENT NOT NULL, team_id INT NOT NULL, name VARCHAR(255) NOT NULL, child_components VARCHAR(10000) NOT NULL, first_lvl_hours INT NOT NULL, second_lvl_hours INT NOT NULL, third_lvl_hours INT NOT NULL, four_lvl_hours INT NOT NULL, five_lvl_hours INT NOT NULL, INDEX IDX_3D65778D296CD8AE (team_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
         $this->addSql('ALTER TABLE qa_components ADD CONSTRAINT FK_3D65778D296CD8AE FOREIGN KEY (team_id) REFERENCES team (id)');
     }
 
