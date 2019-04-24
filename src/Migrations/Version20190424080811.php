f8d5c0dced1e1f0eefe945753f7f540a25869318
ivan.melnichuk <ivan.melnichuk@onyx.com>
2019-04-24 11:08:53 +0300

    Task: https://jira.onyxenterprises.com/browse/WT-74 Description: fix migration

--- src/Migrations/Version20190424080811.php
+++ src/Migrations/Version20190424080811.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20190423065017 extends AbstractMigration
+final class Version20190424080811 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -22,7 +22,9 @@ final class Version20190423065017 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE vacancy DROP INDEX UNIQ_A9346CBD59EC7D60, ADD INDEX IDX_A9346CBD59EC7D60 (assignee_id)');
+        $this->addSql('ALTER TABLE vacancy ADD assignee_id INT DEFAULT NULL, DROP assignee');
+        $this->addSql('ALTER TABLE vacancy ADD CONSTRAINT FK_A9346CBD59EC7D60 FOREIGN KEY (assignee_id) REFERENCES user (id)');
+        $this->addSql('CREATE INDEX IDX_A9346CBD59EC7D60 ON vacancy (assignee_id)');
     }
 
     public function down(Schema $schema) : void
@@ -30,6 +32,8 @@ final class Version20190423065017 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE vacancy DROP INDEX IDX_A9346CBD59EC7D60, ADD UNIQUE INDEX UNIQ_A9346CBD59EC7D60 (assignee_id)');
+        $this->addSql('ALTER TABLE vacancy DROP FOREIGN KEY FK_A9346CBD59EC7D60');
+        $this->addSql('DROP INDEX IDX_A9346CBD59EC7D60 ON vacancy');
+        $this->addSql('ALTER TABLE vacancy ADD assignee VARCHAR(255) DEFAULT NULL COLLATE utf8mb4_unicode_ci, DROP assignee_id');
     }
 }
