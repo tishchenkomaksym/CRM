ed6432145a3f55f78aa5555aca3edf0e502c4417
Oleksandra Havrilova <oleksandra.ha@onyxenterprises.com>
2019-06-04 11:43:17 +0300

    Task: https://jira.onyxenterprises.com/browse/WT-135 Description: add difference column to salaryReportInfo

--- src/Service/SalaryReport/Builder/BaseSalaryReportBuilder.php
+++ src/Service/SalaryReport/Builder/BaseSalaryReportBuilder.php
@@ -11,13 +11,10 @@ namespace App\Service\SalaryReport\Builder;
 
 use App\Entity\SalaryReportInfo;
 use App\Entity\User;
-use App\Service\SalaryReport\Bonuses\Project\ElasticProjectDTOBuilder;
-use App\Service\SalaryReport\Bonuses\User\UserBonusInformationBuilder;
 use App\Service\SalaryReport\Builder\SDTDays\SdtDaysCalculator;
 use App\Service\SalaryReport\Builder\WorkingDays\CalendarWorkingDaysCalculator;
 use App\Service\SalaryReport\SalaryReportDTO;
 use App\Service\User\PhpDeveloper\Hours\BaseWorkHoursInformationBuilder;
-use App\Service\User\PhpDeveloper\Hours\WorkHoursInformation;
 use App\Service\User\Sdt\Filter\AtOwnExpenseFilter;
 use App\Service\User\Sdt\Filter\NotAtOwnExpenseFilter;
 use App\Service\User\Sdt\UsedSdtDaysCalculator;
@@ -44,31 +41,19 @@ class BaseSalaryReportBuilder
      * @var BaseWorkingDaysCalculator
      */
     private $baseWorkingDaysCalculator;
-    /**
-     * @var ElasticProjectDTOBuilder
-     */
-    private $projectDTOBuilder;
-    /**
-     * @var UserBonusInformationBuilder
-     */
-    private $bonusInformationBuilder;
 
     public function __construct(
         CalendarWorkingDaysCalculator $workingDaysCalculator,
         SdtDaysCalculator $sdtDaysCalculator,
         UsedSdtDaysCalculator $usedSdtDaysCalculator,
         BaseWorkingDaysCalculator $baseWorkingDaysCalculator,
-        BaseWorkHoursInformationBuilder $baseWorkHoursInformationBuilder,
-        ElasticProjectDTOBuilder $projectDTOBuilder,
-        UserBonusInformationBuilder $bonusInformationBuilder
+        BaseWorkHoursInformationBuilder $baseWorkHoursInformationBuilder
     ) {
         $this->workingDaysCalculator = $workingDaysCalculator;
         $this->sdtDaysCalculator = $sdtDaysCalculator;
         $this->usedSdtDaysCalculator = $usedSdtDaysCalculator;
         $this->baseWorkHoursInformationBuilder = $baseWorkHoursInformationBuilder;
         $this->baseWorkingDaysCalculator = $baseWorkingDaysCalculator;
-        $this->projectDTOBuilder = $projectDTOBuilder;
-        $this->bonusInformationBuilder = $bonusInformationBuilder;
     }
 
     /**
@@ -83,62 +68,13 @@ class BaseSalaryReportBuilder
         SalaryReportInfo $newReport,
         User $user
     ): SalaryReportDTO {
-
         $returnObject = new SalaryReportDTO();
-        $dateForSdt = $this->getDateForSDT($newReport);
-        $dateWorkingHours = $this->getDateWorkingHours($newReport);
-        /** @noinspection NullPointerExceptionInspection */
-        $previousDateTime = new DateTime("@{$previousReportInfo->getCreateDate()->getTimeStamp()}");
-
-        $this->getUserBonusInformation($user, $previousDateTime, $dateForSdt);
-
-        $returnObject->sdtCountUsed = $this->getSdtCountUsed($previousDateTime, $dateForSdt, $user);
-        $returnObject->sdtCountAtOwnExpenseUsed = $this->getSdtAtOwnExpenseUsedCount($previousDateTime, $dateForSdt,
-            $user);
-        $returnObject->calendarWorkingDays = $this->workingDaysCalculator->calculate($newReport,
-                $user->getCreateDate()) - $returnObject->sdtCountUsed - $returnObject->sdtCountAtOwnExpenseUsed;
-
-        $returnObject->reportWorkingDays = $this->getReportWorkingDays($previousDateTime, $user->getCreateDate(),
-            $dateWorkingHours,
-            $returnObject->sdtCountUsed + $returnObject->sdtCountAtOwnExpenseUsed);
-        $returnObject->sdtCount = $this->sdtDaysCalculator->calculate($dateForSdt, $user) - $returnObject->sdtCountAtOwnExpenseUsed;
-
-        $returnObject->setTimeInfo($this->getTimeInfo($previousDateTime, $dateWorkingHours, $user));
-
-        $returnObject->timeUnlogged = number_format($returnObject->getTimeInfo()->getLoggedTime() -
-            $returnObject->getTimeInfo()->getRequiredTime(), 2);
-
-        $returnObject->user = $user;
-        if($userTeam = $user->getTeam()) {
-            $returnObject->team = $userTeam;
-        }
-        return $returnObject;
-    }
-
-    /**
-     * @param SalaryReportInfo $newReport
-     * @return DateTime
-     * @throws Exception
-     */
-    private function getDateForSDT(
-        SalaryReportInfo $newReport
-    ): DateTime {
         $dateForSdt = new DateTime();
         /** @noinspection NullPointerExceptionInspection */
         $dateForSdt->setTimestamp($newReport->getCreateDate()->getTimestamp());
         $dateForSdt->setDate($dateForSdt->format('Y'), $dateForSdt->format('m'), (int)$dateForSdt->format('d') - 1);
         $dateForSdt->setTime(23, 59, 59);
-        return $dateForSdt;
-    }
 
-    /**
-     * @param SalaryReportInfo $newReport
-     * @return DateTime
-     * @throws Exception
-     */
-    private function getDateWorkingHours(
-        SalaryReportInfo $newReport
-    ): DateTime {
         /**
          * cause calculate working day to date
          */
@@ -147,54 +83,31 @@ class BaseSalaryReportBuilder
         $dateWorkingHours->setTimestamp($newReport->getCreateDate()->getTimestamp());
         $dateWorkingHours->setDate($dateWorkingHours->format('Y'), $dateWorkingHours->format('m'),
             (int)$dateWorkingHours->format('d'));
-        return $dateWorkingHours;
-    }
+        $previousDateTime = new DateTime("@{$previousReportInfo->getCreateDate()->getTimeStamp()}");
 
-    /**
-     * @param $previousDateTime
-     * @param $dateWorkingHours
-     * @param $user
-     * @return WorkHoursInformation
-     * @throws Exception
-     */
-    private function getTimeInfo(
-        $previousDateTime,
-        $dateWorkingHours,
-        $user
-    ): WorkHoursInformation {
-        return $this->baseWorkHoursInformationBuilder->build(
+        $returnObject->sdtCountUsed = $this->getSdtCountUsed($previousDateTime, $dateForSdt, $user);
+        $returnObject->sdtCountAtOwnExpenseUsed = $this->getSdtAtOwnExpenseUsedCount($previousDateTime, $dateForSdt,
+            $user);
+        $returnObject->calendarWorkingDays = $this->workingDaysCalculator->calculate($newReport, $user->getCreateDate()) - $returnObject->sdtCountUsed - $returnObject->sdtCountAtOwnExpenseUsed;
+
+        $returnObject->reportWorkingDays = $this->getReportWorkingDays($previousDateTime, $user->getCreateDate(), $dateWorkingHours,
+            $returnObject->sdtCountUsed + $returnObject->sdtCountAtOwnExpenseUsed);
+        $returnObject->sdtCount = $this->sdtDaysCalculator->calculate($dateForSdt, $user) - $returnObject->sdtCountAtOwnExpenseUsed;
+        $timeInfo = $this->baseWorkHoursInformationBuilder->build(
             $previousDateTime,
             $dateWorkingHours,
             $user
         );
+        $returnObject->setTimeInfo($timeInfo);
+        $returnObject->timeUnlogged = number_format($returnObject->getTimeInfo()->getLoggedTime() -
+            $returnObject->getTimeInfo()->getRequiredTime(), 2);
+
+        $returnObject->user = $user;
+        return $returnObject;
     }
 
-
-    /**
-     * @param User $user
-     * @param DateTime $from
-     * @param DateTime $to
-     * @throws Exception
-     */
-    private function getUserBonusInformation(User $user, DateTime $from, DateTime $to)
+    private function getReportWorkingDays(DateTime $previousDateTime, DateTime $startDate,  DateTime $dateForSdt, int $usedSdt)
     {
-        $projects = $this->projectDTOBuilder->buildArray();
-        $filteredProjects = [];
-        foreach ($projects as $project) {
-            $endDate = $project->getEndDate();
-            if (($from < $endDate && $endDate < $to) || $project->getStatus() !== 'Closed') {
-                $filteredProjects[] = $project;
-            }
-        }
-        $this->bonusInformationBuilder->buildArray($user, $filteredProjects);
-    }
-
-    private function getReportWorkingDays(
-        DateTime $previousDateTime,
-        DateTime $startDate,
-        DateTime $dateForSdt,
-        int $usedSdt
-    ): int {
         if ($previousDateTime < $startDate) {
             $previousDateTime = $startDate;
         }
@@ -229,7 +142,7 @@ class BaseSalaryReportBuilder
         DateTime $previousReportDate,
         DateTime $nowTime,
         User $user
-    ): int {
+    ) {
         return $this->usedSdtDaysCalculator->calculate($previousReportDate,
             $nowTime, (new AtOwnExpenseFilter())->filter($user->getSdt()->toArray()));
 
