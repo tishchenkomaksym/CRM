1a79154e748e2d2437d50dde571029aa5d667669
oleksandra.ha <oleksandra.ha@onyx.com>
2019-06-10 10:35:35 +0300

    Task: https://jira.onyxenterprises.com/browse/WT-183 Description: return sdtEmailAssignee delete

--- src/Controller/SDTEmailAssigneeController.php
+++ src/Controller/SDTEmailAssigneeController.php
@@ -103,26 +103,26 @@ class SDTEmailAssigneeController extends AbstractController
         ]);
     }
 
-//    /**
-//     * @Route("/{id}", name="sdt_email_assignee_delete", methods={"DELETE"})
-//     * @param Request $request
-//     * @param SDTEmailAssignee $sDTEmailAssignee
-//     * @return Response
-//     * @throws NoDataException
-//     */
-//    public function delete(Request $request, SDTEmailAssignee $sDTEmailAssignee): Response
-//    {
-//        if ($this->isCsrfTokenValid('delete' . $sDTEmailAssignee->getId(), $request->request->get('_token'))) {
-//            $entityManager = $this->getDoctrine()->getManager();
-//            $entityManager->remove($sDTEmailAssignee);
-//            $entityManager->flush();
-//        }
-//
-//        if ($sDTEmailAssignee->getUser() === null){
-//            throw new NoDataException('User not found');
-//        }
-//        return $this->redirectToRoute(self::SDT_EMAIL_ASSIGNEE_INDEX, [
-//            'id' => $sDTEmailAssignee->getUser()->getId(),
-//        ]);
-//    }
+    /**
+     * @Route("/{id}", name="sdt_email_assignee_delete", methods={"DELETE"})
+     * @param Request $request
+     * @param SDTEmailAssignee $sDTEmailAssignee
+     * @return Response
+     * @throws NoDataException
+     */
+    public function delete(Request $request, SDTEmailAssignee $sDTEmailAssignee): Response
+    {
+        if ($this->isCsrfTokenValid('delete' . $sDTEmailAssignee->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($sDTEmailAssignee);
+            $entityManager->flush();
+        }
+
+        if ($sDTEmailAssignee->getUser() === null){
+            throw new NoDataException('User not found');
+        }
+        return $this->redirectToRoute(self::SDT_EMAIL_ASSIGNEE_INDEX, [
+            'id' => $sDTEmailAssignee->getUser()->getId(),
+        ]);
+    }
 }
