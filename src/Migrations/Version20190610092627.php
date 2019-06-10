39271805031520974fa200cedb6fc603c08f7c5c
Oleksandra Havrilova <oleksandra.ha@onyxenterprises.com>
2019-06-10 12:29:12 +0300

    Task: https://jira.onyxenterprises.com/browse/WT-188 Description: add DepartmentTeamSdtViewRules migration

--- src/Migrations/Version20190610092627.php
+++ src/Migrations/Version20190610092627.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20190607103527 extends AbstractMigration
+final class Version20190610092627 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -22,8 +22,7 @@ final class Version20190607103527 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('CREATE TABLE department_team_sdt_view_rules (id INT AUTO_INCREMENT NOT NULL, department_id INT NOT NULL, UNIQUE INDEX UNIQ_8420CE7DAE80F5DF (department_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
-        $this->addSql('ALTER TABLE department_team_sdt_view_rules ADD CONSTRAINT FK_8420CE7DAE80F5DF FOREIGN KEY (department_id) REFERENCES department (id)');
+        $this->addSql('CREATE TABLE department_team_sdt_view_rules (id INT AUTO_INCREMENT NOT NULL, team VARCHAR(255) NOT NULL, department VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
     }
 
     public function down(Schema $schema) : void
