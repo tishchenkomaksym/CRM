e908b293d5389fed5cba9ba1f95b5486779a76fa
ivan.melnichuk <ivan.melnichuk@onyx.com>
2019-06-18 10:47:49 +0300

    Task: https://jira.onyxenterprises.com/browse/WT-192 Description: add validation

--- src/Calendar/DateCalculator/UserSubTeamDateCalculator.php
+++ src/Calendar/DateCalculator/UserSubTeamDateCalculator.php
@@ -6,6 +6,8 @@ namespace App\Calendar\DateCalculator;
 use App\Entity\Sdt;
 use App\Entity\UserInfo;
 use App\Service\HolidayService;
+use DateTime;
+use Exception;
 
 class UserSubTeamDateCalculator
 {
@@ -13,10 +15,10 @@ class UserSubTeamDateCalculator
      * @param UserInfo $userInfo
      * @param Sdt $sdt
      * @param HolidayService $holidayService
-     * @throws \Exception
-     * @return \DateTime
+     * @throws Exception
+     * @return DateTime
      */
-    public function getDateWithOffset(UserInfo $userInfo, Sdt $sdt, HolidayService $holidayService) {
+    public function getDateWithOffset(?UserInfo $userInfo, Sdt $sdt, HolidayService $holidayService) {
         if ($userInfo !== null && $userInfo->getSubTeam() === 'Central Tech Support') {
             $endDate = BaseDateCalculator::getDateWithOffset($sdt->getCreateDate(), $sdt->getCount());
         } else {
