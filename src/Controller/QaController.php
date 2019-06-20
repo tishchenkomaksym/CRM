4197aa2cdbc2f0a90c8fd5a157e6222c0fc81ae4
oleksandra.ha <oleksandra.ha@onyx.com>
2019-06-20 15:13:43 +0300

    Task: https://jira.onyxenterprises.com/browse/WT-164 Description: change qa view

--- src/Controller/QaController.php
+++ src/Controller/QaController.php
@@ -2,13 +2,6 @@
 
 namespace App\Controller;
 
-use App\Service\PhpDeveloperTest\Exception\NoExistsNewLevelOfDeveloper;
-use App\Service\PhpDeveloperTest\PhpDeveloperTestBuilderException;
-use App\Service\PhpDeveloperTest\PhpDeveloperTestsInformationBuilder;
-use App\Service\User\PhpDeveloperLevel\EffectiveTime\BaseEffectiveTimeBuilder;
-use App\Service\User\PhpDeveloperLevel\EffectiveTime\NoRequiredHoursException;
-use App\Service\User\PhpDeveloperLevel\EffectiveTime\ProjectEffectiveTimeBuilder;
-use App\Service\User\PhpDeveloperLevel\ProjectEffectiveTime\UserToProjectTimeSpendBuilder;
 use App\Service\UserInformationService;
 use Exception;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
@@ -44,91 +37,43 @@ class QaController extends AbstractController
         );
     }
     /**
-     * @Route("/qa/view/tech/skills", name="qa_view_tech_skills")
+     * @Route("/qa/view/technical/skills", name="qa_view_technical_skills")
      * @param UserInformationService $service
      * @return Response
      * @throws Exception
      */
-    public function techSkills(
+    public function technicalSkills(
         UserInformationService $service
     )
     {
         $user = $this->getUser();
         $qaManager = $service->getQaManager($user);
         return $this->render(
-            'qa_view/techSkills.html.twig',
+            'qa_view/technicalSkills.html.twig',
             [
                 'userLevel' => $service->getPhpUserLevel($user),
                 'qaManagers' => $qaManager
             ]
         );
     }
-
     /**
-     * @IsGranted("ROLE_QA_AGENT")
-     * @Route("/profile/salary-raise", name="salary_raise")
-     * @param PhpDeveloperTestsInformationBuilder $builder
-     * @param BaseEffectiveTimeBuilder $baseEffectiveTimeBuilder
-     * @param ProjectEffectiveTimeBuilder $projectEffectiveTimeBuilder
-     * @param UserToProjectTimeSpendBuilder $projectTimeSpendBuilder
+     * @Route("/qa/view/theoretical/skills", name="qa_view_theoretical_skills")
+     * @param UserInformationService $service
      * @return Response
-     * @throws NoExistsNewLevelOfDeveloper
-     * @throws PhpDeveloperTestBuilderException
-     * @throws NoRequiredHoursException
      * @throws Exception
      */
-    public function salaryRaise(
-        PhpDeveloperTestsInformationBuilder $builder,
-        BaseEffectiveTimeBuilder $baseEffectiveTimeBuilder,
-        ProjectEffectiveTimeBuilder $projectEffectiveTimeBuilder,
-        UserToProjectTimeSpendBuilder $projectTimeSpendBuilder
-    ): Response
+    public function theoreticalSkills(
+        UserInformationService $service
+    )
     {
         $user = $this->getUser();
-        $userProjects = $projectTimeSpendBuilder->build($user);
+        $qaManager = $service->getQaManager($user);
         return $this->render(
-            'profile/salaryRaise.html.twig',
+            'qa_view/theoreticalSkills.html.twig',
             [
-                'tests' => $builder->build($user),
-                'effectiveTime' => $baseEffectiveTimeBuilder->build($user),
-                'projectTime' => $projectEffectiveTimeBuilder->build($user, $userProjects),
-                //                'projects' => $userProjects,
-                'projects' => [],
+                'userLevel' => $service->getPhpUserLevel($user),
+                'qaManagers' => $qaManager
             ]
         );
     }
-
-    /**
-     * @param PhpDeveloperTestsInformationBuilder $builder
-     * @param BaseEffectiveTimeBuilder $baseEffectiveTimeBuilder
-     * @param ProjectEffectiveTimeBuilder $projectEffectiveTimeBuilder
-     * @param UserToProjectTimeSpendBuilder $projectTimeSpendBuilder
-     * @return Response
-     * @throws NoExistsNewLevelOfDeveloper
-     * @throws NoRequiredHoursException
-     * @throws PhpDeveloperTestBuilderException
-     * @throws Exception
-     */
-    public function qaSalaryRaise(
-        PhpDeveloperTestsInformationBuilder $builder,
-        BaseEffectiveTimeBuilder $baseEffectiveTimeBuilder,
-        ProjectEffectiveTimeBuilder $projectEffectiveTimeBuilder,
-        UserToProjectTimeSpendBuilder $projectTimeSpendBuilder
-    ): Response
-    {
-        $user = $this->getUser();
-        $userProjects = $projectTimeSpendBuilder->build($user);
-        return $this->render(
-            'profile/salaryRaise.html.twig',
-            [
-                'tests' => $builder->build($user),
-                'effectiveTime' => $baseEffectiveTimeBuilder->build($user),
-                'projectTime' => $projectEffectiveTimeBuilder->build($user, $userProjects),
-                //                'projects' => $userProjects,
-                'projects' => [],
-            ]
-        );
-    }
-
-
 }
