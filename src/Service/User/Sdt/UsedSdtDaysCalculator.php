3b03e3e4889c5d0315a374ade02d6a2e341caf05
oleksandra.ha <oleksandra.ha@onyx.com>
2019-06-25 16:55:23 +0300

    Task: https://jira.onyxenterprises.com/browse/WT-223 Description: fix UsedSdtCalculator adding logic for subteams

--- src/Service/User/Sdt/UsedSdtDaysCalculator.php
+++ src/Service/User/Sdt/UsedSdtDaysCalculator.php
@@ -53,7 +53,9 @@ class UsedSdtDaysCalculator
             $startDate = new DateTime("@{$startDate->getTimeStamp()}");
 
             $endDate = $this->endDateOfSdtCalculator->calculate($sdt);
-            if ($startPeriod < $startDate && $endPeriod > $endDate) {
+            $startDate->setTime(00, 00, 00);
+            $endDate->setTime(00, 00, 00);
+            if ($startPeriod < $startDate && $endPeriod >= $endDate) {
                 $usedSdt += $this->workingDaysCalculator->getWorkingDaysBetweenDates($startDate, $endDate);
             } elseif ($startPeriod < $startDate && $endPeriod < $endDate && $startDate < $endPeriod) {
                 $usedSdt += $this->workingDaysCalculator->getWorkingDaysBetweenDates($startDate, $endPeriod);
@@ -61,6 +63,40 @@ class UsedSdtDaysCalculator
                 $usedSdt += $this->workingDaysCalculator->getWorkingDaysBetweenDates($startPeriod, $endPeriod);
             } elseif ($startPeriod > $startDate && $endPeriod > $endDate && $endDate > $startPeriod) {
                 $usedSdt += $this->workingDaysCalculator->getWorkingDaysBetweenDates($startPeriod, $endDate);
+            }
+        }
+        return $usedSdt;
+    }
+    /**
+     * @param DateTime $startPeriod
+     * @param DateTime $endPeriod
+     * @param array $sdtArray
+     * @return int
+     * @throws Exception
+     */
+    public function calculateForSpecialSubTeams(DateTime $startPeriod, DateTime $endPeriod, array $sdtArray): int
+    {
+        $usedSdt = 0;
+        $startPeriod->setTime(00, 00, 00);
+        $endPeriod->setTime(00, 00, 00);
+        foreach ($sdtArray as $sdt) {
+            $startDate = $sdt->getCreateDate();
+            if ($startDate === null) {
+                throw new \InvalidArgumentException('no date');
+            }
+            $startDate = new DateTime("@{$startDate->getTimeStamp()}");
+
+            $endDate = $this->endDateOfSdtCalculator->calculate($sdt);
+            $startDate->setTime(00, 00, 00);
+            $endDate->setTime(00, 00, 00);
+            if ($startPeriod < $startDate && $endPeriod > $endDate) {
+                $usedSdt += $startDate->diff(date_modify($endDate, '+1 day'))->days;
+            } elseif ($startPeriod < $startDate && $endPeriod < $endDate && $startDate < $endPeriod) {
+                $usedSdt += $startDate->diff($endPeriod)->days;
+            } elseif ($startPeriod > $startDate && $endPeriod < $endDate && $startDate < $endPeriod) {
+                $usedSdt += $startPeriod->diff($endPeriod)->days;
+            } elseif ($startPeriod > $startDate && $endPeriod > $endDate && $endDate > $startPeriod) {
+                $usedSdt += $startPeriod->diff($endDate)->days;
             }
         }
         return $usedSdt;
